5706c4ae2666bda74b1bca192a8a7038aec1731d
MateuszJaz <mateusz.jaz@outlook.com>
2023-01-30 17:52:07 +0100

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom/client';
-import App, { store } from 'app/App';
+import App from 'app/App';
+import { store } from 'app/store';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
